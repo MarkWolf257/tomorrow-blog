cbf288a87d326ada6454b930acffd3326f918322
MarkWolf257 <sambhunathmeher@gmail.com>
2024-10-25 15:57:32 +0530

    feature: add login UI

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
-import express, { Request, Response } from "express";
-import {authenticateBlogger, createBlogger} from "./handlers/auth";
+import express, { Request, Response } from 'express';
+import {authenticateBlogger, createBlogger} from './handlers/auth';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.use(express.json());
-app.use(express.static("public"));
+app.use('/', express.static('public'));
 
-// app.get('/test', async (req: Request, res: Response) => {
-//     const result = await pool.query("SELECT * FROM blogger_table;");
-//     res.send(result);
-// });
+app.get('/', (req: Request, res: Response) => {
+    res.redirect('/login');
+});
 
-app.post("/create-blogger", createBlogger);
-app.post("/authenticate-blogger", authenticateBlogger);
+app.get('/login', (req: Request, res: Response) => {
+    res.render('login');
+})
+
+app.post('/create-blogger', createBlogger);
+app.post('/authenticate-blogger', authenticateBlogger);
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
